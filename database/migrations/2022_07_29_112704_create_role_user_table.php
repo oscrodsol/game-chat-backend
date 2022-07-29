ff54804bcbe1587a21f740e555de5eea10ca0514
oscrodsol <oscar43521@gmail.com>
2022-07-29 13:51:30 +0200

    Added fields in the role_user migration

--- database/migrations/2022_07_29_112704_create_role_user_table.php
+++ database/migrations/2022_07_29_112704_create_role_user_table.php
@@ -15,7 +15,14 @@ return new class extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+
+            $table->unique(['user_id','role_id']);
         });
     }
 
