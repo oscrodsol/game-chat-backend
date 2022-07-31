b67077437185f94a2a9106b8746b13f92e40b22c
oscrodsol <oscar43521@gmail.com>
2022-08-01 01:23:12 +0200

    feat: Bienvenida url de raiz en api

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/', function(){
+    return 'Welcome!';
+});
+
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/games', [GameController::class, 'getAllGames']);
